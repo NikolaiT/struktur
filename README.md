fdbd424e18e08b21e441ec1239e0d4129d8decaa
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2019-07-12 22:18:33 +0200

    progress in struktur

--- README.md
+++ README.md
@@ -1,68 +1,68 @@
 # Struktur
 
-An abstract algorithm that recognizes a recurring structure in rendered HTML pages.
+An abstract algorithm that recognizes a recurring structure in rendered HTML pages without any manual rules. Outputs JSON.
 
-**Struktur** is the end of web scraping as we know it. I used to spend hours upon hours
-searching smart CSS selectors and XPath queries in order to target the information I 
-wanted to scrape from web pages.
+**Struktur** is the end of web scraping / information extraction as we know it.
 
 With **Struktur**, you never even have to open your Web Dev Console. You simply run 
-**Struktur** on a page that you want to extract data from and **Struktur** automatically
-suggest possible structured information. You select the structure you are interested in 
-give the data variable names. 
+**Struktur** on a page that you want to extract data from and **Struktur** automatically suggest possible structured information.
 
-You only need to create the logic that enters your search terms into input fields. The 
-parsing scraping of data is handled completely by **Struktur**.
-
+Struktur finds collections of items with similar structure. In the overwhelming majority of cases, this is the data you need to extract.
 
 ## Motivation
 
-The main motivation is that defining CSS / Xpath selectors for web scraping projects is a 
-strenous task. The markup of websites changes frequently and the maintaining of those selectors
-is soul crushing.
+There are two reasons why web scraping is painful
 
-Therefore, we need a abstract algorithm that automatically detects relevant structure in 
-html pages. 
+#### 1. CSS Selectors and Xpath queries suck big time
+ 
+Finding good CSS selectors and XPath queries to target information on web sites is a strenuous task. Often the same data is rendered in slightly different markup under different conditions. This means that we often need different versions of CSS selectors / Xpath for the same targeted information. The markup of websites changes frequently and the maintaining of those selectors is soul crushing.
+
+
+![example for naughty HTML](examples/badhtml.png)
+
+#### 2. Getting blocked based on IP address, headers, captchas, missing javascript capabilities
+
+The other painful reality of web scraping is getting blocked by web sites. There is a complete industry with companies such as Distil and Incapsula that develop anti scraping software. In 2019, advanced scraping is almost always done with real browsers that are controlled with software such as puppeteer or nightmarejs.  The reasons for that is that we want to behave like real humans. Real humans do not browse websites with curl. Instead, real humans have javascript enabled and real humans use something that renders HTML/CSS/JS (that thing is called *browser*).
+
+While we can solve the second problem only with more resources, we can solve the first problem algorithmically. The only condition is that we need to render the pages that we want to scrape, therefore we need to scrape using headless browsers.
+
+We need an abstract algorithm that automatically detects relevant structure in rendered web pages.
 
 
 ## What is *relevant* structure?
 
-Relevant structure is a collection (list) of objects which are of interest. 
+Relevant structure is a collection of similar objects which are of interest. 
 
-+ Example 1: The Google SERP page has 10 items that consist of a title, link and snippet.
-+ Example 2: The amazon product search results page list has many products that are rendered in a grid system.
++ The Google Search Engine has 10 results objects that consist of a title, link and snippet.
++ The amazon product search results page has many products that are rendered in a grid system and each share a product image, price and title.
++ News sites present their headlines on a frontpage. Each headline consists of a title (with link to the story), description and potentially author name and publication date.
 
 All those items have a common structure when interpretted visually: They have more or less 
 the same vertical alignment, the same font size, the same html tags and so on.
 
-The huge problem however is that this structure is created dynamically from the interplay of 
-HTML, JavaScript and CSS. This means that the HTML structure does not necessarily need to ressemble
-the visual output.
+The huge problem however is that this structure is created dynamically from the interplay of HTML, JavaScript and CSS. This means that the HTML structure does not necessarily need to resemble the visual output.
 
 
 ## What assumptions does Struktur make?
 
-1. The input of *Struktur* is a website rendered by a modern browser. We will use puppeteer to render 
-	websites.
+1. The input of **Struktur** is a website rendered by a modern browser with javascript support. We will use puppeteer to render websites. 
 2. We assume that structure is what humans consider to be related structure. 
-	+ Identical horizontal/vertical alignment
+	+ Identical horizontal/vertical alignment among objects
 	+ More or less same size of bounding rectangles of the object of interest
-	+ As output we are merily interested in *Links*, *Text* and Images (which are Links) 
-	+ Only structure that takes a major part of the viewport is considered structure
+	+ As output we are merely interested in *Links*, *Text* and Images (which are Links). The output needs to be visible.
+	+ Only structure that takes a major part of the visible viewport is considered structure
 	+ There must be at least N=5 related elements
 
 Those assumptions are important. 
 
-When websites protect themselves against scraping by randomizing class names, injecting dummy HTML 
-content, creating duplicate elements, dynamically altering the contents of the site, they still need
-to create a website that can easily interpreted by the non malicious visitor. We abuse this fact.
+When websites protect themselves against web scraping by randomizing class names, injecting dummy HTML content, creating duplicate elements, dynamically altering the contents of the site, they still need to present a website that can be visually interpreted by the non malicious visitor. **Struktur** makes use of this fact.
 
 ## What does Struktur output?
 
 *Struktur* outputs JSON with the parsed structure from a URL that is was fed. 
 
 
-## Algorithm
+## High level algorithm overview
 
 Take a starting node as input. If no node is given, use the `body` element.
 
@@ -73,82 +73,18 @@ Visit the next node in the tree and check again for N identical elements.
 
 After all candidates have been found, get the bonding boxes of the candidates with `getBoundingClientRect()`
 
-If the bounding boxes vertically and horizontically align and have more or less identical dimensions *and* make up 
+If the bounding boxes vertically and horizontally align and have more or less identical dimensions *and* make up 
 a significant part of `document.body.getBoundingClientRect()`, add those elements to the potential structures.
 
-In the next stage we compare the items within the potential strcutures. If they share *common caracteristics*, we consider
-those elements to form a valid structure.
+In the next stage we compare the items within the potential structures. If they share *common characteristics*, we consider those elements to form a valid structure. We are only interested in `img`, `a` and textNodes. Furthermore, we are only interested in visible textNodes.
 
 
-### Check for identical elements
+## Clustering
 
-```js
+https://github.com/harthur/clustering
 
-(function findCandidates() {
+## Examples
 
-	const N = 5;
-	const minWidth = 200;
-	const minHeight = 200;
-
-	const structure_tags = ['div', 'article', 'p', 'section', 'span'];
-
-	var allElems = document.body.getElementsByTagName('*');
-
-	var candidates = [];
-	var found = [];
-
-	for (element of allElems) {
-		if (element.childElementCount > N) {
-			let child_tags = {};
-			for (var i = 0; i < element.childElementCount; i++) {
-				let tag = element.children[i].tagName.toLowerCase();
-				if (structure_tags.includes(tag)) {
-					child_tags[tag] = child_tags[tag] ? child_tags[tag] + 1 : 1;
-				}
-			}
-
-			for (tag in child_tags) {
-				if (child_tags[tag] > N) {
-					candidates.push({
-						'node': element,
-						'tag': tag,
-					})
-				}
-			}
-		}
-	}
-
-	// remove objects of candidates that do not visually align
-	// horizontically or vertically
-	for (candidate of candidates) {
-
-		var children = Array.from(candidate.node.children);
-		children = children.filter((child) => {
-			return child.tagName.toLowerCase() === candidate.tag;
-		});
-
-		var heights = [];
-		var widths = [];
-
-		for (child of children) {
-			var rect = child.getBoundingClientRect();
-			if (rect.width > minWidth) {
-				widths.push(rect.width);
-			}
-			if (rect.height > minHeight) {
-				heights.push(rect.height);
-			}
-		}
-
-		if (heights.length > 0 && heights.every( e => e === heights[0] )) {
-			found.push(children);
-		}
-
-		if (widths.length > 0 && widths.every( e => e === widths[0] )) {
-			found.push(children);
-		}
-	}
-
-	return found;
-})();
-```
+1. [Struktur explores the Google SERP](examples/google.png)
+2. [Struktur finds articles on NYT](examples/nyt.png)
+3. [All amazon products are detected by Struktur](examples/amaonz.png)
--- struktur.js
+++ struktur.js
@@ -1,160 +1,240 @@
 (() => {
 
-    function findCandidates() {
-        const N = 5;
-        const minWidth = 200;
-        const minHeight = 200;
-        const structure_tags = ['div', 'article', 'p', 'section', 'span', 'aside', 'p'];
+    Array.prototype.extend = function (other_array) {
+        /* You should include a test to check whether other_array really is an array */
+        other_array.forEach(function(v) {this.push(v)}, this);
+    };
 
-        var allElems = document.body.getElementsByTagName('*');
+    class Struktur {
 
-        var candidates = [];
-        var found = [];
+        constructor(config) {
+            this.config = {
+                N: 6,
+                minWidth: 200,
+                minHeight: 200,
+                minWidthMin: 10,
+                minHeightMin: 10,
+                structureTags: ['div', 'article', 'p', 'section', 'span', 'aside', 'p']
+            };
 
-        for (let element of allElems) {
-            if (element.childElementCount > N) {
-                let child_tags = {};
-                for (var i = 0; i < element.childElementCount; i++) {
-                    let tag = element.children[i].tagName.toLowerCase();
-                    if (structure_tags.includes(tag)) {
-                        child_tags[tag] = child_tags[tag] ? child_tags[tag] + 1 : 1;
+            Object.assign(this.config, config);
+        }
+
+        /**
+         * Run struktur in the current DOM.
+         */
+        struktur() {
+            let structures = this.findCandidates();
+            console.log(structures);
+            let data = {};
+
+            for (let s = 0; s < structures.length; s++) {
+                let structure = structures[s];
+                let structure_name = 'structure-' + s;
+                data[structure_name] = [];
+                for (let node of structure) {
+                    let parsed = this.parseContent(node);
+                    data[structure_name] = parsed;
+                }
+            }
+
+            console.dir(data);
+        }
+
+        /**
+         * Remove structures from the list with objects with zero parsed
+         * information elements.
+         *
+         */
+        filterStructures() {
+
+        }
+
+        /**
+         * Find all candidates that are potentially containing a structure.
+         *
+         * @returns {Array}
+         */
+        findCandidates() {
+
+            var allElems = document.body.getElementsByTagName('*');
+
+            var candidates = [];
+            var found = [];
+
+            for (let element of allElems) {
+                if (element.childElementCount > this.config.N) {
+                    let child_tags = {};
+                    for (var i = 0; i < element.childElementCount; i++) {
+                        let tag = element.children[i].tagName.toLowerCase();
+                        if (this.config.structureTags.includes(tag)) {
+                            child_tags[tag] = child_tags[tag] ? child_tags[tag] + 1 : 1;
+                        }
+                    }
+
+                    for (let tag in child_tags) {
+                        if (child_tags[tag] > this.config.N) {
+                            candidates.push({
+                                'node': element,
+                                'tag': tag,
+                            })
+                        }
+                    }
+                }
+            }
+
+            // remove objects of candidates that do not visually align
+            // horizontally or vertically
+            for (let candidate of candidates) {
+
+                var children = Array.from(candidate.node.children);
+                children = children.filter((child) => {
+                    return child.tagName.toLowerCase() === candidate.tag;
+                });
+
+                var heights = [];
+                var widths = [];
+
+                for (let child of children) {
+                    var rect = child.getBoundingClientRect();
+                    if (rect.width > this.config.minWidth) {
+                        widths.push(rect.width);
+                    }
+                    if (rect.height > this.config.minHeight) {
+                        heights.push(rect.height);
                     }
                 }
 
-                for (let tag in child_tags) {
-                    if (child_tags[tag] > N) {
-                        candidates.push({
-                            'node': element,
-                            'tag': tag,
-                        })
-                    }
+                if (heights.length >= this.config.N && heights.every(e => e === heights[0])) {
+                    found.push(children);
+                }
+
+                if (widths.length > this.config.N && widths.every(e => e === widths[0])) {
+                    found.push(children);
                 }
             }
+
+            return found;
         }
 
-        // remove objects of candidates that do not visually align
-        // horizontally or vertically
-        for (let candidate of candidates) {
 
-            var children = Array.from(candidate.node.children);
-            children = children.filter((child) => {
-                return child.tagName.toLowerCase() === candidate.tag;
-            });
-
-            var heights = [];
-            var widths = [];
-
-            for (let child of children) {
-                var rect = child.getBoundingClientRect();
-                if (rect.width > minWidth) {
-                    widths.push(rect.width);
-                }
-                if (rect.height > minHeight) {
-                    heights.push(rect.height);
-                }
-            }
-
-            if (heights.length > 0 && heights.every(e => e === heights[0])) {
-                found.push(children);
-            }
-
-            if (widths.length > 0 && widths.every(e => e === widths[0])) {
-                found.push(children);
-            }
-        }
-
-        return found;
-    }
-
-
-    /*
-     * Parses visible content in the single elements of the found structure.
-     *
-     * What is content?
-     *
-     * Every semantic tag such as span, a, em, strong, time, small
-     * where o.children.length == 0 and with o.innerText
-     *
-     * Check that the style.display attribute is not set to 'none'
-     *
-     */
-    function parseContent(container) {
-
-        var parsed = [];
-
-        function walkTheDOM(node, func) {
-            func(node);
+        walkTheDOM(node, array) {
+            array.push(node);
             node = node.firstChild;
             while (node) {
-                walkTheDOM(node, func);
+                this.walkTheDOM(node, array);
                 node = node.nextSibling;
             }
         }
 
-        function extractReadable(currentNode) {
+
+        /**
+         *
+         * Find all text nodes within node.
+         * Source: https://stackoverflow.com/questions/10730309/find-all-text-nodes-in-html-page
+         *
+         * @param el
+         * @returns {Array}
+         */
+        textNodesUnder(el) {
+            var n, a=[], walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT,null,false);
+            while(n = walk.nextNode()) {
+                a.push(n);
+            }
+            return a;
+        }
+
+
+        /**
+         * https://stackoverflow.com/questions/19669786/check-if-element-is-visible-in-dom
+         *
+         * @param elem
+         * @returns {boolean} Whether the element is visible by the user.
+         */
+        isVisible(elem) {
+            const style = getComputedStyle(elem);
+
+            if (style.display === 'none') return false;
+
+            if (style.visibility !== 'visible') return false;
+
+            if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
+                elem.getBoundingClientRect().width === 0) {
+                return false;
+            }
+
+            return true;
+        }
+
+        extractReadable(currentNode) {
 
             if (currentNode.tagName === 'A' && currentNode.hasAttribute('href')) {
                 let link = currentNode.getAttribute('href');
                 if (currentNode.innerText && currentNode.innerText.length > 0) {
-                    parsed.push({
+                    this.usedTextNodes.extend(this.textNodesUnder(currentNode));
+                    this.parsed.push({
                         'href': link,
                         'linkText': currentNode.innerText.trim()
-                    })
-                } else {
-                    // parsed.push({
-                    //     'href': link
-                    // })
+                    });
                 }
             }
 
             if (currentNode.tagName === 'IMG' && currentNode.hasAttribute('src')) {
                 let src = currentNode.getAttribute('src');
                 if (currentNode.hasAttribute('alt')) {
-                    parsed.push({
+                    this.parsed.push({
                         'type': 'img',
                         'src': src,
                         'imgAlt': currentNode.getAttribute('alt').trim(),
                     })
                 } else {
-                    parsed.push({
+                    this.parsed.push({
                         'type': 'img',
                         'src': src
                     })
                 }
             }
 
-            if (currentNode.nodeType === 3) {
-                let text = currentNode.data.trim();
-                if (text.length > 1) {
-                    parsed.push({
-                        'text': currentNode.data.trim()
-                    });
+            if (currentNode.nodeType === 3 && !this.usedTextNodes.includes(currentNode)) {
+                let parent = currentNode.parentElement;
+                if (this.isVisible(parent)) {
+                    let text = currentNode.data.trim();
+                    if (text.length > 1) {
+                        this.parsed.push({
+                            'text': currentNode.data.trim()
+                        });
+                    }
                 }
             }
-
         }
 
-        walkTheDOM(container, extractReadable);
+        /*
+         * Parses visible content in the single elements of the found structure.
+         *
+         * What is content?
+         *
+         * Every semantic tag such as span, a, em, strong, time, small
+         * where o.children.length == 0 and with o.innerText
+         *
+         * Check that the style.display attribute is not set to 'none'
+         *
+         */
+        parseContent(container) {
+            this.parsed = [];
+            this.usedTextNodes = [];
+            let allNodes = [];
 
-        return parsed;
-    }
+            this.walkTheDOM(container, allNodes);
 
+            for (let node of allNodes) {
+                this.extractReadable(node);
+            }
 
-    let structures = findCandidates();
-    console.log(structures);
-
-    // run everything
-    let data = [];
-    for (var s = 0; s < structures.length; s++) {
-        let structure = structures[s];
-        let structure_name = 'structure-'+s;
-        data[structure_name] = [];
-        for (let node of structure) {
-            data[structure_name].push(parseContent(node));
+            return this.parsed;
         }
+
     }
 
-    console.dir(data);
-    //console.log(JSON.stringify(data, null, 2));
+    (new Struktur()).struktur();
 
 })();
